fc0c32acab811b6f90ff67dd8bb23ba67b63f3af
Costin Leau <cleau@vmware.com>
2012-07-16 14:59:18 +0300

    add hive commands

--- plugin-hive/src/main/java/org/springframework/data/hadoop/impala/hive/HiveCommands.java
+++ plugin-hive/src/main/java/org/springframework/data/hadoop/impala/hive/HiveCommands.java
@@ -15,26 +15,114 @@
  */
 package org.springframework.data.hadoop.impala.hive;
 
-import org.springframework.data.hadoop.impala.common.ConfigurationAware;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.data.hadoop.hive.HiveClientFactoryBean;
+import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 /**
- * Commands to submit and interact with MapReduce jobs
+ * Provider of Hive commands.
  * 
- *
+ * @author Costin Leau
  */
 @Component
-public class HiveCommands extends ConfigurationAware {
+public class HiveCommands implements ApplicationContextAware, CommandMarker {
 
-	@Override
-	public boolean configurationChanged() {
-		return true;
+	private static final String PREFIX = "hive ";
+
+	@Autowired
+	private Configuration hadoopConfiguration;
+
+	private String host = "localhost";
+	private Integer port = 10000;
+	private Long timeout = TimeUnit.MINUTES.toMillis(2);
+
+	private HiveClientFactoryBean hiveClientFactory;
+
+	private ResourcePatternResolver resourceResolver;
+
+	@PostConstruct
+	public void init() throws Exception {
+		hiveClientFactory = new HiveClientFactoryBean();
+
+		hiveClientFactory.setAutoStartup(false);
+		hiveClientFactory.setHost(host);
+		hiveClientFactory.setPort(port);
+		hiveClientFactory.setTimeout(timeout.intValue());
 	}
 
-	@CliCommand(value = "hive script", help = "run hive script")
-	public void submit(@CliOption(key = { "scriptfile" }, mandatory = true, help = "the hive script file") final String scriptFile) {
-		throw new UnsupportedOperationException();
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.resourceResolver = applicationContext;
+	}
+
+
+	@CliCommand(value = { PREFIX + "info" }, help = "Returns basic info about the Hive configuration")
+	public String info() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hive [");
+		String hiveVersion = HiveConf.class.getPackage().getImplementationVersion();
+		sb.append((StringUtils.hasText(hiveVersion) ? hiveVersion : "unknown"));
+		sb.append("][host=");
+		sb.append(host);
+		sb.append("][port=");
+		sb.append(port);
+		sb.append("]");
+		// TODO: potentially add a check to see whether HDFS is running
+
+		return sb.toString();
+	}
+
+	@CliCommand(value = { PREFIX + "cfg" }, help = "Configures Hive")
+	public void config(@CliOption(key = { "host" }, mandatory = false, help = "Server host") String host, 
+			@CliOption(key = { "port" }, mandatory = false, help = "Server port") Integer port, 
+			@CliOption(key = { "timeout" }, mandatory = false, help = "Connection Timeout") Long timeout)
+			throws Exception {
+		
+		if (StringUtils.hasText(host)) {
+			this.host = host;
+		}
+		if (port != null) {
+			this.port = port;
+		}
+		if (timeout != null) {
+			this.timeout = timeout;
+		}
+	}
+
+	@CliCommand(value = { PREFIX + "script" }, help = "Executes a Hive script")
+	public void script(@CliOption(key = { "", "location" }, mandatory = true, help = "Script location") String location)
+			throws Exception {
+
+		Resource resource = resourceResolver.getResource(location);
+		Assert.isTrue(resource.exists(), "No resource found at " + location);
+
+		// for each run, start a new Hive instance
+		init();
+
+		hiveClientFactory.setScripts(Collections.singleton(resource));
+
+		try {
+			hiveClientFactory.afterPropertiesSet();
+			hiveClientFactory.start();
+		} finally {
+			hiveClientFactory.destroy();
+		}
 	}
 }
